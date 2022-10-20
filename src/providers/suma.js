7f58fd7bac6fd643c67bf494f605009112d15a3b
emiliano-hc-seguridata <ehidalgo@seguridata.com>
2022-10-20 08:56:33 -0500

    try/catch

--- src/providers/suma.js
+++ src/providers/suma.js
@@ -1,6 +1,6 @@
 const { WebVerification } = require("vdid-sdk-web");
 
-class SumaProvider{
+class SumaProvider {
   uuid = '';
   token = '';
   vdid = new WebVerification("pk_test_GngHeqnzIO/6Qlsb0frz/CQLBgxZJloDWC2JHsgHO70=");
@@ -8,61 +8,71 @@ class SumaProvider{
 
   verificationEp = async () => {
     // Llamada al EP createVerification
-    const response = await fetch(
-      "https://veridocid.azure-api.net/api/id/v2/createVerification",
-      {
-        method: "POST",
-        headers: {
-          "x-api-key": "sk_test_lAbhn2YI41ZIIsZUQckTwBB6WuFu1R6G+FKE4IutMg0=",
-        },
-        body: JSON.stringify({
-          id: "test",
-          options: {
-            checks: {
-              selfie: false,
-            },
-            redirect_url: "https://verificaciones.sumamexico.com/",
+    try {
+      const response = await fetch(
+        "https://veridocid.azure-api.net/api/id/v2/createVerification",
+        {
+          method: "POST",
+          headers: {
+            "x-api-key": "sk_test_lAbhn2YI41ZIIsZUQckTwBB6WuFu1R6G+FKE4IutMg0=",
           },
-        }),
-      }
-    );
+          body: JSON.stringify({
+            id: "test",
+            options: {
+              checks: {
+                selfie: false,
+              },
+              redirect_url: "https://verificaciones.sumamexico.com/",
+            },
+          }),
+        }
+      );
 
-    if (response.ok) {
-      const uuidIdentifier = await response.text();
-      this.uuid = uuidIdentifier;
-      console.log(uuidIdentifier);
-      //Se obtiene el uuid y se pasa para poder crear la verificación
-      this.vdid.verifyIdentity(uuidIdentifier, { method: 'popup' });
-    } else {
-      const error = await response.json();
-      console.log("error", error);
+      if (response.ok) {
+        const uuidIdentifier = await response.text();
+        this.uuid = uuidIdentifier;
+        console.log(uuidIdentifier);
+        //Se obtiene el uuid y se pasa para poder crear la verificación
+        this.vdid.verifyIdentity(uuidIdentifier, { method: 'popup' });
+      } else {
+        const error = await response.json();
+        console.log("error", error);
+      }
+    }
+    catch (error) {
+      console.log("Verefication error:" + error);
     }
   };
 
   getStatus = async () => {
     // Llamada al EP createVerification
-    const response = await fetch(
-      "https://veridocid.azure-api.net/api/id/v2/status",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": "Bearer " + this.token,
-        },
-        body: JSON.stringify({
-          uuid: "16a7c56e-4083-43e9-87f0-f390c5bc9eaa",
-        })
-      }
-    );
-    console.log(this.uuid);
+    try {
+      const response = await fetch(
+        "https://veridocid.azure-api.net/api/id/v2/status",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + this.token,
+          },
+          body: JSON.stringify({
+            uuid: "16a7c56e-4083-43e9-87f0-f390c5bc9eaa",
+          })
+        }
+      );
+      console.log(this.uuid);
 
-    if (response.ok) {
-      const uuidIdentifier = await response.text();
-      console.log(uuidIdentifier);
-      //Se obtiene el uuid y se pasa para poder crear la verificación
-    } else {
-      const error = await response.json();
-      console.log("error", error);
+      if (response.ok) {
+        const uuidIdentifier = await response.text();
+        console.log(uuidIdentifier);
+        //Se obtiene el uuid y se pasa para poder crear la verificación
+      } else {
+        const error = await response.json();
+        console.log("error", error);
+      }
+    }
+    catch (error) {
+      alert("getStatus: " + error);
     }
   };
 
@@ -87,7 +97,7 @@ class SumaProvider{
     // @ts-ignore
     fetch("https://veridocid.azure-api.net/api/auth/token", requestOptions)
       .then(response => response.json())
-      .then(result =>{ this.token = result["access_token"]; console.log(result);})
+      .then(result => { this.token = result["access_token"]; console.log(result); })
       .catch(error => console.log('error', error));
   };
 
